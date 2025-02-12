bdd92fcf6812e98c09d279c8d3b0619ef12191e0
Shivamtomar007 <nips3034tomar@gmail.com>
2025-02-12 11:17:01 +0530

    backend

--- Backened/createServer/index.js
+++ Backened/createServer/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const PORT = 2560;
 const server = http.createServer((req,res)=>{
 res.write("Welcome to Node Server");
-res.end("Request Completed");
+res.end(" Request Completed");
 })
 server.listen(PORT,()=>{
 console.log("First Server is running on port "+PORT);
